5ff73d7434794c5e10045bc04f6ba5ea045dcc72
Dharma Yudistira <dharmayudistira2000@gmail.com>
2025-01-28 19:32:19 +0700

    fix

--- src/utils/dropdown.tsx
+++ src/utils/dropdown.tsx
@@ -46,8 +46,8 @@ function Dropdown() {
                 <div className="dropdown absolute z-50 right-0 mt-2 w-56 origin-top-right rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     {/* List item options */}
                     <div className="py-1">
-                        {dropdownList.map(data => (
-                            <button onClick={() => console.log(data)} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{data}</button>
+                        {dropdownList.map((data, index) => (
+                            <button key={index} onClick={() => console.log(data)} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{data}</button>
                         ))}
                     </div>
                 </div>
